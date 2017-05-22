49896c07e189c80a7a64de8d61f222cd988107ef
elmong <vill0312@algonquinlive.com>
2017-05-22 13:50:07 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/*global alert, console*/
+
+
+
+
+var a = 3;
+var b = 5;
+var c;
+
+alert("var a = 3;\nvar 8 =5;\nvar c;\n_________________________\n" + 
+     "a + b = " +(a + b) + "\n" +
+     "a - b = " +(a - b) + "\n" +
+     "a * b = " +(a * b) + "\n" +
+     "a / b = " +(a / b) + "\n" +
+     "a % b = " +(a % b) + "\n" +
+     "(a += b): " +(a += b) + "\n" +
+     "(a -= b): " +(a -= b) + "\n" +
+     "(a *= b): " +(a *= b) + "\n" +
+     "(a /= b): " +(a /= b) + "\n" +
+     "(a %= b): " +(a %= b) + "\n" +
+     "(a == b): " +(a == b) + "\n" +
+     "(a != b): " +(a != b) + "\n" +
+     "(a > b): " +(a > b) + "\n" +
+     "(a < b): " +(a < b) + "\n" +
+     "(!a && !c): " +(!a && !c) + "\n" +
+     "(!a || !c): " +(!a || !c) + "\n"
+     );
+
+
+
+var first_name = "Elmon John";
+var last_name = "Villena";
+var email = "elmon.villena@gmail.com";
+
+var output;
+
+
+
+
+
+
+
+
